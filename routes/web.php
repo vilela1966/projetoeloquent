df8a3404037f79d0b9f966c1f67d0583af4919fe
vilela1966 <airton.vilela@gmail.com>
2017-02-15 20:45:16 -0200

    acrescentando listagem e detalhes da editora

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rota para url '/editoras'
+Route::get('/editoras', 'EditoraController@index');
+
+// Rota para url 'editoras/1', por exemplo
+Route::get('/editoras/{editora}', 'EditoraController@show');
